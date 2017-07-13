e94b76c86a1f24d0571692e314d2741557f2f44a
CultOfApe <ted.project.bcn@gmail.com>
2017-07-14 00:36:03 +0200

    Templates - simple dialogue system
    An attempt to modularize the dialogue system in "Phil Troy", so I can reuse in other games. Has some glitches still, when picking replies most prominently, will fix tomorrow :)

---
+++ Templates - simple dialogue system/dialogue.gd
@@ -0,0 +1,93 @@
+extends Node2D
+
+var npc_dialogue = {"dialogue": "res://npc1.json", "branch": "a"}
+var talk_data = {}
+
+onready var dialog_panel = load("res://dialogue_window.tscn")
+onready var reply_button = load("res://reply.tscn")
+
+onready var viewsize = get_viewport().get_rect().size
+
+var num_replies
+
+func _ready():
+	set_process(true)
+	set_fixed_process(true)
+	set_process_input(true)
+	
+	for object in get_node("npcs").get_children():
+		object.connect("dialogue", self, "_talk_to")
+		object.connect("look_at", self, "_look_at")
+
+	start_dialogue(npc_dialogue)
+
+func _talk_to(dialogue, branch):
+	npc_dialogue = {"dialogue": dialogue, "branch": branch}
+	start_dialogue(npc_dialogue)
+
+func _pick_reply(n):
+	if talk_data["dialogue"][npc_dialogue["branch"]]["responses"][n]["next"] != "exit":
+		npc_dialogue["branch"] = talk_data["dialogue"][npc_dialogue["branch"]]["responses"][n]["next"]
+		start_dialogue(npc_dialogue)
+	else:
+		kill_dialogue()
+	
+func start_dialogue(json):
+	load_json(json, "dialogue")
+	num_replies = talk_data["dialogue"][npc_dialogue["branch"]]["responses"].size()
+	
+	#setup dialog window
+	dialogue_window()
+	
+	#set text and reply in dialogue panel
+	get_node("ui_dialogue/Panel/dialogue").set_text(talk_data["dialogue"][npc_dialogue["branch"]]["text"])
+	for n in range(0,num_replies):
+		get_node("ui_dialogue/Panel/reply" + str(n+1)).set_text(talk_data["dialogue"][npc_dialogue["branch"]]["responses"][n]["reply"])
+		
+func load_json(json, type):
+	var file = File.new();
+	file.open(json["dialogue"], File.READ);
+	talk_data.parse_json(file.get_as_text())
+	file.close()
+
+func kill_dialogue():
+	for x in get_node("ui_dialogue/Panel").get_children():
+		x.set_name("DELETED") #to make sure node doesn´t cause issues before being deleted
+		x.queue_free()
+
+func dialogue_window():
+	var reply_offset = 0
+	var labels = ["dialogue"]
+	
+	#add one element "reply" per number of replies in game_data
+	for n in range(num_replies):
+		labels.push_back("reply" + str(n+1))
+		
+	new_label(labels)
+	
+	var dialog = {"width": 800, "height": 100, "posx": 400, "posy": 370}
+	
+	get_node("ui_dialogue/Panel").set_size(Vector2(dialog.width, dialog.height + num_replies*30))
+	get_node("ui_dialogue/Panel").set_pos(Vector2(viewsize.x/2 - dialog.posx, viewsize.y - dialog.posy))
+	get_node("ui_dialogue/Panel").set_opacity(0.5)
+	
+	for n in range(num_replies):
+		get_node("ui_dialogue/Panel/reply" + str(n+1)).set_size(Vector2(400, 50))
+		get_node("ui_dialogue/Panel/reply" + str(n+1)).set_pos(Vector2(viewsize.x/2 - dialog.posx-100, viewsize.y - 550 + reply_offset))
+		get_node("ui_dialogue/Panel/reply" + str(n+1)).num_reply = n
+		reply_offset += 30
+
+	reply_offset = 0
+
+func new_label(labels):
+	for lbl in labels:
+		#if I want the panel to be instanced too, instead of always visible
+#		if lbl == "panel":
+#			var node = dialog_panel.instance()
+#			node.set_name(lbl)
+#			get_node("Panel/ui_dialogue").add_child(node)
+#		else:
+		var node = reply_button.instance()
+		node.set_name(lbl)
+		node.connect("reply_selected",self,"_pick_reply",[], CONNECT_ONESHOT)
+		get_node("ui_dialogue/Panel").add_child(node)
---
+++ Templates - simple dialogue system/npc2.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+signal look_at(a)
+signal dialogue(a,b)
+
+func _ready():
+	pass
+
+func _on_npc2_input_event( viewport, event, shape_idx ):
+	if event.type == InputEvent.MOUSE_BUTTON and event.button_index == BUTTON_LEFT:
+		if event.is_pressed():
+			emit_signal("dialogue", "res://npc2.json","a")
+
+	if event.type == InputEvent.MOUSE_BUTTON and event.button_index == BUTTON_RIGHT and event.is_pressed():
+		emit_signal("look_at", "This is an NPC")
